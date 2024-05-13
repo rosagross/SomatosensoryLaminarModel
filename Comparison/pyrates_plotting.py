d0106687b7f4fea64e3aa503ccb60ce45ec92d4a
susanne_holtzsch <susanne.holtzsch@gmail.com>
2024-05-13 12:01:07 +0200

    Plots

--- Comparison/pyrates_plotting.py
+++ Comparison/pyrates_plotting.py
@@ -21,26 +21,31 @@ python_4 = pd.read_csv('output/python_4.csv')
 pyrates_4 = pd.read_csv('output/pyrates_4.csv')
 
 # %%
+time_r = np.linspace(0,2,len(python_10)+1)
+time = np.linspace(0,2,len(python_10))
+
 fig, ax = plt.subplots(4,1)
-ax[0].set_title('16')
-ax[0].plot(python['S1'][:], label='python')
-ax[0].plot(pyrates['S1'][:], label='pyrates')
+fig.suptitle('Difference between PyRates and pure Python for example of S1')
+ax[0].set_title('16 cells')
+ax[0].plot(time, python['S1'][:], label='python')
+ax[0].plot(time_r, pyrates['S1'][:], label='pyrates')
 
-ax[1].set_title('15')
-ax[1].plot(python_15['S1'][:], label='python')
-ax[1].plot(pyrates_15['S1'][:], label='pyrates')
+ax[1].set_title('15 cells')
+ax[1].plot(time, python_15['S1'][:], label='python')
+ax[1].plot(time_r, pyrates_15['S1'][:], label='pyrates')
 
-ax[2].set_title('11')
-ax[2].plot(python_11['S1'][:], label='python')
-ax[2].plot(pyrates_11['S1'][:], label='pyrates')
+ax[2].set_title('11 cells')
+ax[2].plot(time, python_11['S1'][:], label='python')
+ax[2].plot(time_r, pyrates_11['S1'][:], label='pyrates')
   
-ax[3].set_title('10')
-ax[3].plot(python_10['S1'][:], label='python')
-ax[3].plot(pyrates_10['S1'][:], label='pyrates')
+ax[3].set_title('10 cells')
+ax[3].plot(time, python_10['S1'][:], label='python')
+ax[3].plot(time_r, pyrates_10['S1'][:], label='pyrates')
 
 # ax[4].set_title('4')
 # ax[4].plot(python_4['E3'][:], label='python')
 # ax[4].plot(pyrates_4['E3'][:], label='pyrates')
+plt.tight_layout(rect=[0, 0, 1, 0.96])
 plt.legend()
 plt.show()
 
@@ -48,15 +53,10 @@ plt.show()
 #Unterschied erst zwischen 11 & 16 bei E4, P4
 
 # %%
-import matplotlib.pyplot as plt
-
 cells = ['E1', 'E2', 'E3', 'E4', 'P1', 'P2', 'P3', 'P4', 'S1', 'S2']
 
 fig, ax = plt.subplots(5, 2, figsize=(10, 20))
 fig.suptitle('PyRates(--) vs pure Python(-) with 10, 11 and 16 populations')
-
-time_r = np.linspace(0,2,len(python_10)+1)
-time = np.linspace(0,2,len(python_10))
 
 for i, cell in enumerate(cells):
     row = i // 2
@@ -75,6 +75,7 @@ for i, cell in enumerate(cells):
 
     ax[0, 0].legend()
 
-plt.tight_layout(h_pad=7)
+plt.tight_layout(rect=[0, 0, 1, 0.96])
+plt.subplots_adjust(hspace=0.8)
 plt.savefig('output/compare.pdf') 
 plt.show()
