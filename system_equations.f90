18a3c3950fec843d9a89c190e1b5bc315e9eacde
rosagross <grossmann.rc@gmail.com>
2025-09-02 08:52:42 +0200

    Updates (discovered that jax version produces different output!)

--- system_equations.f90
+++ system_equations.f90
@@ -14,23 +14,23 @@ implicit none
 
 double precision, intent(in) :: t
 double precision, intent(in) :: y(8)
-double precision :: v(1)
-double precision :: x(1)
-double precision :: v_v1(1)
-double precision :: x_v1(1)
-double precision :: v_v2(1)
-double precision :: x_v2(1)
-double precision :: v_v3(1)
-double precision :: x_v3(1)
-double precision :: m_v2(1)
-double precision :: m_in_v2(1)
-double precision :: m(1)
-double precision :: m_in_v3(1)
-double precision :: m_v1(1)
-double precision :: m_in(1)
-double precision :: m_in_v1(1)
+double precision :: v
+double precision :: x
+double precision :: v_v1
+double precision :: x_v1
+double precision :: v_v2
+double precision :: x_v2
+double precision :: v_v3
+double precision :: x_v3
+double precision :: m_v2
+double precision :: m_in_v2
+double precision :: m
+double precision :: m_in_v3
+double precision :: m_v1
+double precision :: m_in
+double precision :: m_in_v1
 double precision, intent(inout) :: dy(8)
-double precision, intent(in) :: u(1)
+double precision, intent(in) :: u
 double precision, intent(in) :: tau
 double precision, intent(in) :: h
 double precision, intent(in) :: tau_v1
@@ -54,14 +54,14 @@ double precision, intent(in) :: weight
 double precision, intent(in) :: weight_v1
 
 
-v = y(1:1)
-x = y(2:2)
-v_v1 = y(3:3)
-x_v1 = y(4:4)
-v_v2 = y(5:5)
-x_v2 = y(6:6)
-v_v3 = y(7:7)
-x_v3 = y(8:8)
+v = y(1)
+x = y(2)
+v_v1 = y(3)
+x_v1 = y(4)
+v_v2 = y(5)
+x_v2 = y(6)
+v_v3 = y(7)
+x_v3 = y(8)
 m_v2 = m_max/(exp(s*(-v + v_thr - v_v1)) + 1)
 m_in_v2 = m_v2*weight_v2
 m = m_max_v1/(exp(s_v1*(v_thr_v1 - v_v2)) + 1)
@@ -70,14 +70,14 @@ m_v1 = m_max_v2/(exp(s_v2*(v_thr_v2 - v_v3)) + 1)
 m_in = m*weight
 m_in_v1 = m_v1*weight_v1
 
-dy(1:1) = x
-dy(2:2) = h*(m_in + u)/tau - 2*x/tau - v/tau**2
-dy(3:3) = x_v1
-dy(4:4) = h_v1*m_in_v1/tau_v1 - 2*x_v1/tau_v1 - v_v1/tau_v1**2
-dy(5:5) = x_v2
-dy(6:6) = h_v2*m_in_v2/tau_v2 - 2*x_v2/tau_v2 - v_v2/tau_v2**2
-dy(7:7) = x_v3
-dy(8:8) = h_v3*m_in_v3/tau_v3 - 2*x_v3/tau_v3 - v_v3/tau_v3**2
+dy(1) = x
+dy(2) = h*(m_in + u)/tau - 2*x/tau - v/tau**2
+dy(3) = x_v1
+dy(4) = h_v1*m_in_v1/tau_v1 - 2*x_v1/tau_v1 - v_v1/tau_v1**2
+dy(5) = x_v2
+dy(6) = h_v2*m_in_v2/tau_v2 - 2*x_v2/tau_v2 - v_v2/tau_v2**2
+dy(7) = x_v3
+dy(8) = h_v3*m_in_v3/tau_v3 - 2*x_v3/tau_v3 - v_v3/tau_v3**2
 
 end subroutine
 
@@ -109,7 +109,7 @@ integer, intent(in) :: ndim
 double precision, intent(inout) :: y(ndim), args(*)
 double precision, intent(in) :: t
 
-args(1) = 220.0  ! u
+args(1) = 400.0  ! u
 args(2) = 0.01  ! tau
 args(3) = 0.00325  ! h
 args(4) = 0.02  ! tau_v1
@@ -131,13 +131,13 @@ args(24) = 560.0  ! s_v2
 args(25) = 5.0  ! m_max_v2
 args(26) = 108.0  ! weight
 args(27) = 33.75  ! weight_v1
-y(1) = 0.0  ! v
+y(1) = 0.03052549  ! v
 y(2) = 0.0  ! x
-y(3) = 0.0  ! v_v1
+y(3) = -0.02195986  ! v_v1
 y(4) = 0.0  ! x_v1
-y(5) = 0.0  ! v_v2
+y(5) = 0.01762243  ! v_v2
 y(6) = 0.0  ! x_v2
-y(7) = 0.0  ! v_v3
+y(7) = 0.004405607  ! v_v3
 y(8) = 0.0  ! x_v3
 
 end subroutine stpnt
