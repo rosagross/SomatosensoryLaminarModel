c25e84d17d16ef5fe3c2245eb86edef1338ef328
rosagross <grossmann.rc@gmail.com>
2025-10-20 18:21:01 +0200

    prepare simulation for hpc runs

--- hpc_scripts/run_simulation.sh
+++ hpc_scripts/run_simulation.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=prepro_step004
+#SBATCH --job-name=simulation_run
 #SBATCH --time=00:30:00
 #SBATCH --mem=8000
 #SBATCH --cpus-per-task=5
-#SBATCH --array=13-52
+#SBATCH --array=10, 20, 30, 40, 50
 #SBATCH --mail-user=grossmannr@cbs.mpg.de
-#SBATCH --output=/ptmp/grossmannr/nt_detect/logs/prepro/%A/%x_%A_%a.log
-#SBATCH --error=/ptmp/grossmannr/nt_detect/logs/prepro/%A/%x_%A_%a.log
+#SBATCH --output=/ptmp/grossmannr/somato_model/logs/prepro/%A/%x_%A_%a.log
+#SBATCH --error=/ptmp/grossmannr/somato_model/logs/prepro/%A/%x_%A_%a.log
 
 export PYTHONUNBUFFERED=TRUE
 
-export RAWDIR=/ptmp/grossmannr/nt_detect/rawdir                    # Set environment variables
-export DATADIR=/ptmp/grossmannr/nt_detect/datadir 
-export RESDIR=/ptmp/grossmannr/nt_detect/results_grossmannr
-export WDDIR=/u/grossmannr/NearThresholdDetection
+export SIMDIR=/ptmp/grossmannr/somato_model/results_grossmannr
+export WDDIR=/u/grossmannr/SomatosensoryLaminarModel
 
-srun /u/grossmannr/miniforge3/envs/pyrates_env/bin/python /u/grossmannr/SomatosensoryLaminarModel/simulation_main.py --coupling $SLURM_ARRAY_TASK_ID
+srun /u/grossmannr/miniforge3/envs/pyrates__env/bin/python /u/grossmannr/SomatosensoryLaminarModel/simulation_main.py --coupling $SLURM_ARRAY_TASK_ID
