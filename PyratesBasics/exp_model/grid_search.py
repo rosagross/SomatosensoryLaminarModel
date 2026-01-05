implemented grid search without class model, still wrong results starting from the second parameter!

PyratesBasics/exp_model/grid_search.py
@@ -1,33 +1,38 @@
-# %%
-""" Complete PyRates model with the parameters from "parameters.py" and definitions compatible with PyCobi continuations. 
-- background input in all non-thalamic populations
-- time-varying external input in thalE
-- connectivity parameters (g/bEI) 
+"""
+File: grid_search.py
+Author: Rosa Grossmann
+Contact: grossmannr@cbs.mpg.de
+Date: 2026-01-05
+Description: Grid search based on circuit template implementation. 
 """
 
-import os 
-import sys
 
+# %%
+import time
+import os 
 WDDIR = os.getenv("WDDIR")
 SIMDIR = os.getenv("SIMDIR")
 os.chdir(os.path.join(WDDIR,"PyratesBasics","exp_model")) 
-
+import sys
 from pyrates.frontend import OperatorTemplate, NodeTemplate, EdgeTemplate, CircuitTemplate
+from pyrates import grid_search
 from copy import deepcopy
-#from parameters import Parameter
+from complete_model_class import SomatoModelPyrates
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from numba import njit
-from yaml_saving import circuit_to_yaml
 from pprint import pprint
-## import dei parametri
+import json
 param_path = os.path.join(WDDIR,"Simulations")
+
 if param_path not in sys.path:
     sys.path.append(param_path)
 from parameters import Parameter
+import json
 
-#%%
+# %% 
+# Set-Up the model 
 # Parameters:
 cells = ['E3b','PV3b','SST3b','VIP3b', # A3b
         'E1S1', 'PV1S1', 'SST1S1', 'VIPS1', 'E2S1', 'PV2S1', 'SST2S1', 'E3S1', 'PV3S1', 'SST3S1', 'E4S1', 'PV4S1', 'SST4S1', # S1
@@ -329,10 +334,27 @@ model = CircuitTemplate(
     path = None)
 
 # %%
-circuit_to_yaml(model, "expanded_model.yaml")
+# read pyrates model from yaml circuit file
+start = time.time()
+pmdl = CircuitTemplate.from_yaml("expanded_model.yaml/model")
+end = time.time()
+print("loading time:", start-end)
 
+# %% Simulation parameters
+with open(os.path.join(WDDIR, 'Simulations', 'simulation_parameter.json'), 'r') as json_file:
+    params = json.load(json_file)
+    
 # %%
-# Run the simulation 
+param_grid = {'background_input': np.array([0, 5])} # values to "sweep" (defined as dictionary)  / the name is not related to the PyRates definition
+param_map = {
+    'background_input': {
+        'vars': ['BACKGROUND/RPO_bI/bI'],
+        'nodes': ['BACKGROUND']
+    }
+}
+
+# %% 
+N_cells = len(cells)
 rpo_names_extended = rpo_names + ['RPO_Iext']
 outputs = {}
 b_inputs = []
@@ -344,131 +366,176 @@ for i, target_cell in enumerate(cells):
         for rpo_name in rpo_names[:N_cells]:
             outputs[f'V_{target_cell}/{rpo_name}'] = f'{target_cell}/{rpo_name}/v'
 
+    # Include rpo_names_bI only if i in range(N_cells-2)
+    #if i in range(N_cells-2):
+        #print(f'{target_cell}/RPO_bI/v')
 outputs['V_background/RPO_bI'] = 'BACKGROUND/RPO_bI/v_bI'
 
-results = model.run(simulation_time=simulation_time,
-                  step_size=step_size,
-                  sampling_step_size=sampling_step_size,
-                  outputs=outputs,
-                  backend ="scipy",
-                  vectorize=False,
-                  clear=False,
-                  float_precision="float64"
-                  #decorator=njit
-                  )
+# %%
+results, results_map = grid_search(circuit_template=model,
+                                   param_grid=param_grid,
+                                   param_map=param_map,
+                                   simulation_time=simulation_time,
+                                   step_size=step_size,
+                                   sampling_step_size=sampling_step_size,
+                                   #inputs={'PC/RPO_e_in/u': noise},
+                                   inputs = None, # the input is inside the model
+                                   outputs=outputs,
+                                   backend="scipy",
+                                   permute_grid=True,
+                                   vectorize=False,
+                                   float_precision="float64"
+                                   #solver="euler",
+                                   #cutoff=cutoff
+                                   )
 
-# %% Pandas Dataframe
-all_potentials = []
+# structure of "results": nested Pandas Dataframe with a multi-level column index
+# 1) outputs requested in "grid_search"
+# 2) simulation keys (parameters sweep runs)
+# 3) node and variables of the model
 
-for i, cell in enumerate(cells):
+# %%
+"""
+fig, axes = plt.subplots(nrows=results_map.shape[0], figsize=(8, 12))
+results_map.sort_values('input_duration', inplace=True, axis=0)
+
+for ax, key in zip(axes, results_map.index):
+    all_potentials_param = []
+    for i, cell in enumerate(cells):
     # always include rpo_names[:N_cells]
-    if i == (N_cells-2): # thalE
-        potential_keys = [f'V_{cell}/{rpo}' for rpo in rpo_names_extended[:N_cells+1]]
-    else: 
-        potential_keys = [f'V_{cell}/{rpo}' for rpo in rpo_names[:N_cells]]
+        if i == (N_cells-2):
+            potential_keys = [f'V_{cell}/{rpo}' for rpo in rpo_names_extended[:N_cells+1]]
+        else: 
+            potential_keys = [f'V_{cell}/{rpo}' for rpo in rpo_names[:N_cells]]
 
     # include rpo_name only if i in range(N_cells-2)
-    if i in range(N_cells-2):
-        potential_keys += [f'V_background/RPO_bI']
+        if i in range(N_cells-2):
+            potential_keys += [f'V_{cell}/RPO_bI']
 
-    # include rpo_names_extended[:N_cells+1] only if i == 13
-    
-    sources = results[potential_keys]
-    all_potentials.append(np.sum(sources, axis=1))
-
-all_potentials = np.array(all_potentials).T
-
-potential_df = pd.DataFrame(all_potentials, columns=cells)
-
-#potential_df.to_csv("/data/hu_mecozzi/Documents/SomatosensoryLaminarModel/PyratesBasics/exp_model/simulations/a3bthal.csv") 
-
-# %% Rates 
-n_timepoints, n_cells = all_potentials.shape
-m_out_all = np.zeros((n_timepoints, n_cells))  # NumPy array, not list!
-
-for i, source_cell in enumerate(cells):
-    m_out_all[:, i] =  m_max[i] / (
-        1 + np.exp(r[i] * (v_thr[i] - all_potentials[:, i]))
-    )
-
-rates_df = pd.DataFrame(m_out_all, columns=cells)
-# %% plot
-
-# division into layers
+        idx = pd.IndexSlice
+        sources = results.loc[:, idx[potential_keys, key]]
+        all_potentials_param.append(np.sum(sources, axis=1))
 """
-layers = [potential_df.columns[:4],   # A3b 
-          potential_df.columns[4:17],  # S1
-          potential_df.columns[17:30],  # S2
-          potential_df.columns[30:32] # thal
-         ]
-"""
-layers = [ # i_ext + bckgrd
-    ["E1S1","E2S1","E3S1","E4S1"],
-    ["E1S2","E2S2","E3S2","E4S2"],
-    ["thalE","thalI"], # thalamus
-    ["PV1S1","PV2S1","PV3S1","PV4S1"],
-    ["PV1S2","PV2S2","PV3S2","PV4S2"],
-    potential_df.columns[:4], # A3b
-    ["SST1S1","SST2S1","SST3S1","SST4S1"],
-    ["SST1S2","SST2S2","SST3S2","SST4S2"],
-    ["VIPS1"],
-    ["VIPS2"]
+
+par_name1 = results_map.columns[0]
+#par_name2 = results_map.columns[1]
+layer_splits = [
+    [i for i, c in enumerate(cells) if c.endswith("3b")],   # A3b
+    [i for i, c in enumerate(cells) if c.endswith("S1")],   # S1
+    [i for i, c in enumerate(cells) if c.endswith("S2")],   # S2
+    [i for i, c in enumerate(cells) if c in ["thalE", "thalI"]]  # Thalamus
 ]
 
-"""fig, axes = plt.subplots(2, 2, figsize=(14, 10))  """
-fig, axes = plt.subplots(4, 3, figsize=(18, 16))
-axes = axes.flatten()
-for ax, cols in zip(axes, layers):
-    labels_with_final = [f"{col} ({potential_df[col].iloc[-1]:.6f})" for col in cols]
-    # Plot and override legend labels
-    potential_df[cols].plot(ax=ax, legend=False)  # suppress default legend
-    ax.legend(labels_with_final, loc="best")
-    ax.set_ylabel("[mV]")
-    ax.set_xlabel("samples")
-    ax.set_title(", ".join(cols))
-#axes[-1].set_visible(False)
-fig.suptitle("Potentials", fontsize=20, fontweight='bold')
+
+fig, axes = plt.subplots(nrows=results_map.shape[0], ncols=len(layer_splits), figsize=(14, 4*results_map.shape[0]), sharex=True)
+#results_map.sort_values('input_duration', inplace=True, axis=0)
+#results_map.sort_values('input_strength', inplace=True, axis=0)
+#results_map.sort_values('background_input', inplace=True, axis=0)
+
+if results_map.shape[0] == 1:
+    axes = np.array([axes])  
+
+idx = pd.IndexSlice
+for row_axes, key in zip(axes, results_map.index):
+    all_potentials_param = []
+    names = []
+    
+    for i, cell in enumerate(cells):
+        if i == (N_cells-2):
+            potential_keys = [f'V_{cell}/{rpo}' for rpo in rpo_names_extended[:N_cells+1]]
+        else: 
+            potential_keys = [f'V_{cell}/{rpo}' for rpo in rpo_names[:N_cells]]
+
+        if i in range(N_cells-2):
+            potential_keys += [f'V_background/RPO_bI']
+
+        sources = results.loc[:, idx[potential_keys, key]]
+        summed = np.sum(sources, axis=1)
+        all_potentials_param.append(summed)
+        names.append(f"{cell} ({summed.iloc[-1]:.6f})")
+
+    for ax, layer_slice in zip(row_axes, layer_splits):
+        layer_series = [all_potentials_param[i] for i in layer_slice]
+        layer_names  = [names[i] for i in layer_slice]
+
+        for series, label in zip(layer_series, layer_names):
+            ax.plot(series.index, series.values, label=label)
+
+        ax.set_ylabel("[mV]")
+        ax.set_xlabel("seconds")
+        ax.legend(loc="best")
+        
+        #value = results_map.loc[key, par_name]*step_size # input amplitude
+        value1 = results_map.loc[key, par_name1] 
+        #value2 = results_map.loc[key, par_name2]
+        ax.set_title(f"{par_name1}: {value1:.4f}")
+        #ax.set_title(f"{par_name1}: {value1:.4f} / {par_name2}: {value2:.4f}")
+        
+
+#fig.suptitle(f"Potentials: {par_name1}/{par_name2}", fontsize=20, fontweight='bold')
+fig.suptitle(f"Potentials: {par_name1}", fontsize=20, fontweight='bold')
 plt.tight_layout(rect=[0, 0, 1, 0.95])
 plt.show()
 
-# %% plot - rates
+# %%
 
-# division into layers
-"""
-layers = [rates_df.columns[:4],   # A3b 
-          rates_df.columns[4:17],  # S1
-          potential_df.columns[17:30],  # S2
-          potential_df.columns[30:32] # thal
-         ]
-"""
-layers = [ # i_ext + bckgrd
-    ["E1S1","E2S1","E3S1","E4S1"],
-    ["E1S2","E2S2","E3S2","E4S2"],
-    ["thalE","thalI"], # thalamus
-    ["PV1S1","PV2S1","PV3S1","PV4S1"],
-    ["PV1S2","PV2S2","PV3S2","PV4S2"],
-    rates_df.columns[:4], # A3b
-    ["SST1S1","SST2S1","SST3S1","SST4S1"],
-    ["SST1S2","SST2S2","SST3S2","SST4S2"],
-    ["VIPS1"],
-    ["VIPS2"]
-]
-#fig, axes = plt.subplots(2, 2, figsize=(14, 10))  # 2x2 grid
-fig, axes = plt.subplots(4, 3, figsize=(18, 16))
-axes = axes.flatten()
+results_grid1 = results.loc[:, idx[:, 'model_0']]
+results_grid2 = results.loc[:, idx[:, 'model_1']]
+#results_grid3 = results.loc[:, idx[:, 'model_2']]
 
-for ax, cols in zip(axes, layers):
-    labels_with_final = [f"{col} ({rates_df[col].iloc[-1]:.6f})" for col in cols]
-    
-    rates_df[cols].plot(ax=ax,legend=False)
-    ax.set_title(", ".join(cols))  # show which cols are in this subplot
-    ax.legend(labels_with_final, loc="best")
-    ax.set_ylabel("[Hz]")
-    ax.set_xlabel("samples")
-    
-#axes[-1].set_visible(False)
-fig.suptitle("Rates", fontsize=20, fontweight='bold')
-plt.tight_layout(rect=[0, 0, 1, 0.95])
-plt.show() 
+# read in simulations from class
+results_class0 = pd.read_csv(os.path.join(param_path, 'simulation_results_class_pyrates.csv')).iloc[:2000]
+results_class5 = pd.read_csv(os.path.join(param_path, 'simulation_results_class_pyrates_b5.csv')).iloc[:2000] 
+results_class7 = pd.read_csv(os.path.join(param_path, 'simulation_results_class_pyrates_b7.csv')).iloc[:2000]
+
+# compare results from grid search with class simulations
+results_grid1.columns = list(outputs.keys())
+results_grid2.columns = list(outputs.keys())
+#results_grid3.columns = list(outputs.keys())
+
+# %%
+diff_class_grid0 = pd.DataFrame(
+    results_class0.to_numpy() - results_grid1.to_numpy(),
+    index=results_grid2.index,
+    columns=results_grid2.columns
+)
+
+plt.plot(diff_class_grid0['V_E3b/RPO_E3b'])
+plt.plot(diff_class_grid0['V_E3b/RPO_PV3b'])
+plt.plot(diff_class_grid0['V_E3b/RPO_SST3b'])
+
+# %%
+diff_class_grid5 = pd.DataFrame(
+    results_class5.to_numpy() - results_grid2.to_numpy(),
+    index=results_grid1.index,
+    columns=results_grid1.columns
+)
+
+plt.plot(diff_class_grid5['V_E3b/RPO_E3b'])
+plt.plot(diff_class_grid5['V_E3b/RPO_PV3b'])
+plt.plot(diff_class_grid5['V_E3b/RPO_SST3b'])
+
+# %%
+diff_class_grid7 = pd.DataFrame(
+    results_class7.to_numpy() - results_grid3.to_numpy(),
+    index=results_grid2.index,
+    columns=results_grid2.columns
+)
+
+plt.plot(diff_class_grid7['V_E3b/RPO_E3b'])
+plt.plot(diff_class_grid7['V_E3b/RPO_PV3b'])
+plt.plot(diff_class_grid7['V_E3b/RPO_SST3b'])
+
+# %%
+
+diff_grid_grid = pd.DataFrame(
+    results_grid1.to_numpy() - results_grid2.to_numpy(),
+    index=results_grid1.index,
+    columns=results_grid1.columns
+)
+
+plt.plot(diff_grid_grid['V_E3b/RPO_E3b'])
+plt.plot(diff_grid_grid['V_E3b/RPO_PV3b'])
+plt.plot(diff_grid_grid['V_E3b/RPO_SST3b'])
 
 # %%