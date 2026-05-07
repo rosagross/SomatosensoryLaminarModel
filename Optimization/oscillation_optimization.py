b2743af64b93700dbb8431427648e3bc62f4c771
rosagross <grossmann.rc@gmail.com>
2026-05-07 13:07:46 +0200

    start optimization script

---
+++ Optimization/oscillation_optimization.py
@@ -0,0 +1,22 @@
+"""
+File: oscillation_optimization.py
+Author: Rosa Grossmann
+Contact: grossmannr@cbs.mpg.de
+Date: 2026-04-20
+Description: In this script we run simulations using the fully connected model. 
+After each simulation run I compute the frequency pattern of the activity outcome and 
+
+
+"""
+
+import 
+
+# %%
+
+# Simulation 
+
+
+
+
+# %% 
+# Optimization
